9a10871e25ae3e41e5af72c7bed10a18f9008432
ciberpunk3r <ciberpunk3r@gmail.com>
2022-06-12 23:09:44 -0500

    🎉🥂 Initial commit

---
+++ src/routes/products.routes.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import {
+  getProducts,
+  createNewProduct,
+  getProductById,
+  deleteProductById,
+  getTotalProducts,
+  updateProductById,
+} from "../controllers/products.controller";
+
+const router = Router();
+
+console.log("🛸 Entering products.routes.js");
+
+router.get("/products", getProducts);
+
+router.post("/products", createNewProduct);
+
+router.get("/products/count", getTotalProducts);
+
+router.get("/products/:id", getProductById);
+
+router.delete("/products/:id", deleteProductById);
+
+router.put("/products/:id", updateProductById);
+
+export default router;
